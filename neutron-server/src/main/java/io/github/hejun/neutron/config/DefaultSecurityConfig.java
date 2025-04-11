4a5e28ccf4c74fdb8a378cd63c6ae83c80c7cc44
HeJun <1255524587@qq.com>
2025-04-11 23:59:11 +0800

    处理警告问题

--- neutron-server/src/main/java/io/github/hejun/neutron/config/DefaultSecurityConfig.java
+++ neutron-server/src/main/java/io/github/hejun/neutron/config/DefaultSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -25,18 +24,14 @@ public class DefaultSecurityConfig {
 	public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(authorize ->
-				authorize.anyRequest().authenticated()
+				authorize
+					.requestMatchers("/style/**", "/script/**", "/favicon.ico", "/robots.txt").permitAll()
+					.anyRequest().authenticated()
 			)
 			.formLogin(formLogin ->
 				formLogin.loginPage("/login").permitAll()
 			);
 		return http.build();
-	}
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring()
-			.requestMatchers("/style/**", "/script/**", "/favicon.ico", "/robots.txt");
 	}
 
 }
