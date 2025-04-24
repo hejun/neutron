1cf8a0020fe1e6f8526bf481d6c5b4d04a9701d2
HeJun <1255524587@qq.com>
2025-04-24 18:00:24 +0800

    封装请求

--- neutron-ui/src/views/Callback.vue
+++ neutron-ui/src/views/Callback.vue
@@ -1,57 +1,32 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router'
 import useAuthStore from '@/store/AuthStore'
-import { enc, SHA256 } from 'crypto-js'
+import { enc } from 'crypto-js'
+import { obtainToken, signIn } from '@/api/AuthorizationApi.ts'
 
 const router = useRouter()
 const route = useRoute()
 const authStore = useAuthStore()
 
-const BASE_URL = import.meta.env.VITE_BASE_URL ?? ''
-const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? ''
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI ?? ''
-const SCOPE = import.meta.env.VITE_SCOPE ?? ''
-
 const { error, error_description, code, state, callbackUrl } = route.query
 
 const oAuthSignIn = () => {
-  authStore.initAuthentication(callbackUrl ? enc.Base64.stringify(enc.Utf8.parse(callbackUrl as string)) : undefined)
-
-  const codeVerify = authStore.authentication!.codeVerify
-  const state = authStore.authentication!.state!
-  const nonce = authStore.authentication!.nonce!
-  const codeChallenge = enc.Base64url.stringify(SHA256(codeVerify))
-
-  window.location.href = `${BASE_URL}/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&code_challenge_method=S256&code_challenge=${codeChallenge}&state=${state}&nonce=${nonce}`
+  signIn(callbackUrl ? (callbackUrl as string) : undefined)
 }
-const obtainToken = () => {
-  const codeVerify = authStore.authentication!.codeVerify!
+
+const oAuthObtainToken = () => {
+  const codeVerify = authStore.authentication ? authStore.authentication!.codeVerify : authStore.authentication
+
+  if (!code || !codeVerify) {
+    // eslint-disable-next-line no-alert
+    alert(`Missing code or verify code`)
+    return
+  }
+
   authStore.destroyAuthentication()
 
-  const url = `${BASE_URL}/oauth2/token`
-  const params = {
-    grant_type: 'authorization_code',
-    code: code as string,
-    client_id: CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
-    code_verifier: codeVerify as string
-  }
-  const body = new URLSearchParams(Object.entries(params))
-
-  fetch(url, { method: 'POST', body })
-    .then(res => res.json())
-    .then(res => {
-      const expireAt = new Date()
-      expireAt.setSeconds(expireAt.getSeconds() + res.expires_in)
-      const token = {
-        accessToken: res.access_token,
-        expiresAt: expireAt.getTime(),
-        idToken: res.id_token,
-        scope: res.scope,
-        tokenType: res.token_type
-      }
-      authStore.updateToken(token)
-    })
+  obtainToken(code as string, codeVerify)
+    .then(res => authStore.updateToken(res))
     .then(() => {
       const url = state ? decodeURIComponent(enc.Utf8.stringify(enc.Base64.parse(state as string))) : '/'
       router.replace(url)
@@ -62,7 +37,7 @@ const obtainToken = () => {
 <template>
   <div>
     <button type="button" @click="oAuthSignIn">OAuth SignIn</button>
-    <button type="button" @click="obtainToken">Obtain Token</button>
+    <button type="button" @click="oAuthObtainToken">Obtain Token</button>
   </div>
   <div>
     <p>{{ error }}</p>
--- neutron-ui/src/views/Home.vue
+++ neutron-ui/src/views/Home.vue
@@ -1,33 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import useAuthStore from '@/store/AuthStore'
 import { RouterView } from 'vue-router'
-
-const authStore = useAuthStore()
-
-const BASE_URL = import.meta.env.VITE_BASE_URL ?? ''
-
-const oAuthSignOut = () => {
-  authStore.destroyToken()
-  const url = `${BASE_URL}/logout`
-  window.location.replace(url)
-}
+import { findCurrentUserinfo, signOut } from '@/api/AuthorizationApi.ts'
 
 const userinfoRef = ref()
+
 const loadUserinfo = () => {
-  fetch(`${BASE_URL}/userinfo`, {
-    method: 'GET',
-    headers: { Authorization: `${authStore.token?.tokenType} ${authStore.token?.accessToken}` }
-  })
-    .then(res => res.json())
-    .then(res => (userinfoRef.value = res))
+  findCurrentUserinfo().then(res => (userinfoRef.value = res))
 }
 </script>
 
 <template>
   <div>
     <button type="button" @click="loadUserinfo">Load Userinfo</button>
-    <button type="button" @click="oAuthSignOut">OAuth SignOut</button>
+    <button type="button" @click="signOut">OAuth SignOut</button>
   </div>
   <div>{{ userinfoRef }}</div>
   <RouterView />
