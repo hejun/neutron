36d30817801a5068cd301b1e8f932b43556e2da1
HeJun <1255524587@qq.com>
2025-05-07 15:44:33 +0800

    版本升级

--- neutron-ui/vite.config.ts
+++ neutron-ui/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
