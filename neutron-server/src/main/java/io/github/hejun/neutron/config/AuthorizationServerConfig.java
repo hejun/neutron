5fdbf4c4224dbdb518fc7b32e35f463b0f1e6726
HeJun <1255524587@qq.com>
2025-08-13 16:30:19 +0800

    自定义Token返回信息

--- neutron-server/src/main/java/io/github/hejun/neutron/config/AuthorizationServerConfig.java
+++ neutron-server/src/main/java/io/github/hejun/neutron/config/AuthorizationServerConfig.java
@@ -1,7 +1,12 @@
 package io.github.hejun.neutron.config;
 
 import com.nimbusds.jwt.JWTParser;
+import io.github.hejun.neutron.entity.Client;
+import io.github.hejun.neutron.entity.Tenant;
 import io.github.hejun.neutron.security.issuer.AuthorizationServerContextEnhanceConfigurer;
+import io.github.hejun.neutron.service.IClientService;
+import io.github.hejun.neutron.service.ITenantService;
+import io.github.hejun.neutron.util.ContextUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +17,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.oidc.authentication.OidcLogoutAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.oidc.web.authentication.OidcLogoutAuthenticationConverter;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationConverter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -38,35 +46,9 @@ public class AuthorizationServerConfig {
 
 	@Bean
 	@Order(Ordered.HIGHEST_PRECEDENCE)
-	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http,
-																	  RegisteredClientRepository registeredClientRepository) throws Exception {
+	public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http, RegisteredClientRepository registeredClientRepository) throws Exception {
 		OAuth2AuthorizationServerConfigurer configurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
-		http
-			.securityMatcher(configurer.getEndpointsMatcher())
-			.with(configurer, authorizationServer ->
-				authorizationServer
-					.authorizationEndpoint(authorizationEndpoint ->
-						authorizationEndpoint.consentPage("/consent")
-					)
-					.oidc(oidc ->
-						oidc
-							.logoutEndpoint(logoutEndpoint ->
-								logoutEndpoint
-									.logoutRequestConverter(createOidcLogoutAuthenticationConverter(registeredClientRepository))
-							)
-					)
-			)
-			.authorizeHttpRequests(authorizeRequests ->
-				authorizeRequests.anyRequest().authenticated()
-			)
-			.cors(Customizer.withDefaults())
-			.exceptionHandling(exceptionHandling ->
-				exceptionHandling
-					.defaultAuthenticationEntryPointFor(
-						new LoginUrlAuthenticationEntryPoint("/login"),
-						new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-					)
-			);
+		http.securityMatcher(configurer.getEndpointsMatcher()).with(configurer, authorizationServer -> authorizationServer.authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint.consentPage("/consent")).oidc(oidc -> oidc.logoutEndpoint(logoutEndpoint -> logoutEndpoint.logoutRequestConverter(createOidcLogoutAuthenticationConverter(registeredClientRepository))))).authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated()).cors(Customizer.withDefaults()).exceptionHandling(exceptionHandling -> exceptionHandling.defaultAuthenticationEntryPointFor(new LoginUrlAuthenticationEntryPoint("/login"), new MediaTypeRequestMatcher(MediaType.TEXT_HTML)));
 		// 自有 AuthorizationServerContext 缺少信息, 禁用原有的,使用自定义增强添加
 		http.with(new AuthorizationServerContextEnhanceConfigurer(), Customizer.withDefaults());
 		return http.build();
@@ -90,15 +72,30 @@ public class AuthorizationServerConfig {
 					if (registeredClient != null && !CollectionUtils.isEmpty(registeredClient.getPostLogoutRedirectUris())) {
 						String postLogoutRedirectUri = registeredClient.getPostLogoutRedirectUris().stream().findFirst().orElse(null);
 						if (postLogoutRedirectUri != null) {
-							authentication = new OidcLogoutAuthenticationToken(authentication.getIdTokenHint(),
-								(Authentication) authentication.getPrincipal(), authentication.getSessionId(),
-								authentication.getClientId(), postLogoutRedirectUri, authentication.getState());
+							authentication = new OidcLogoutAuthenticationToken(authentication.getIdTokenHint(), (Authentication) authentication.getPrincipal(), authentication.getSessionId(), authentication.getClientId(), postLogoutRedirectUri, authentication.getState());
 						}
 					}
 				}
 			}
 
 			return authentication;
+		};
+	}
+
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> jwtOAuth2TokenCustomizer(ITenantService tenantService,
+																		   IClientService clientService) {
+		return context -> {
+			if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+				Tenant tenant = tenantService.findByIssuer(ContextUtils.getIssuer());
+				if (tenant != null) {
+					context.getClaims().claim("iss_id", tenant.getId());
+					Client client = clientService.findByClientId(tenant.getId(), ContextUtils.getClientId());
+					if (client != null) {
+						context.getClaims().claim("aud_id", client.getId());
+					}
+				}
+			}
 		};
 	}
 
